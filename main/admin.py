f63552e450df0acb088f5a21ddf0c4b6a653a959
anvarbek009 <anvarasqarov2009@gmail.com>
2024-07-20 11:45:51 +0500

    commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,103 @@
 from django.contrib import admin
-
+from .models import CategoryTransition, PaymentType,Transaction,Wallet
+from datetime import datetime
 # Register your models here.
+
+
+class CategoryTransitionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(CategoryTransition,CategoryTransitionAdmin)
+
+
+class PaymentTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'image')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+    save_on_top = True
+    list_per_page = 20
+    readonly_fields = ('created_at',)
+    def save_model(self, request, obj, form, change):
+        if not obj.id:
+            obj.created_at = datetime.now()
+        obj.save()
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(user=request.user)
+
+admin.site.register(PaymentType, PaymentTypeAdmin)
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'category_transition', 'payment_type', 'date', 'amount', 'description', 'created_at')
+    search_fields = ('user__name', 'category_transition__name', 'payment_type__name', 'date', 'amount', 'description')
+    list_filter = ('created_at',)
+    list_editable = ('amount', 'description')
+    date_hierarchy = 'date'
+    ordering = ('-created_at',)
+    autocomplete_fields = ['user', 'category_transition', 'payment_type']
+    readonly_fields = ('created_at',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.id:
+            obj.user = request.user
+        obj.save()
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(user=request.user)
+    
+    def has_change_permission(self, request, obj=None):
+        if obj:
+            return request.user.is_superuser or obj.user == request.user
+        else:
+            return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        if obj:
+            return request.user.is_superuser or obj.user == request.user
+        else:
+            return request.user.is_superuser
+    
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+admin.site.register(Transaction, TransactionAdmin)
+
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('user', 'balance')
+    search_fields = ('user__name', 'balance')
+    list_filter = ('user__created_at',)
+    autocomplete_fields = ['user']
+    readonly_fields = ('balance',)
+    save_on_top = True
+    list_per_page = 20
+
+    def save_model(self, request, obj, form, change):
+        if not obj.id:
+            obj.user = request.user
+        obj.save()
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(user=request.user)
+
+
+    def has_change_permission(self, request, obj=None):
+        if obj:
+            return request.user.is_superuser or obj.user == request.user
+        else:
+            return request.user.is_superuser
+
+admin.site.register(Wallet, WalletAdmin)        
--- main/models.py
+++ main/models.py
@@ -1,13 +1,61 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
-
-class PaymentType(models.Model):
+class CategoryTransition(models.Model):
     name = models.CharField(max_length=100)
-    image=models.ImageField(upload_to='paymenttypes/', blank=True,null=True)
 
     def __str__(self):
         return self.name
     
     class Meta:
+        db_table = 'category_transitions'
+
+class PaymentType(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=100, decimal_places=2,default=0)
+    image=models.ImageField(upload_to='paymenttypes/', blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.name} - {self.balance}'
+    
+    class Meta:
         db_table = 'payment_types'
+
+class CategoryThings(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='category_things/')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'category_things'
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_transition = models.ForeignKey(CategoryTransition, on_delete=models.CASCADE)
+    payment_type = models.ForeignKey(PaymentType, on_delete=models.CASCADE)
+    date = models.DateField()
+    image = models.ImageField(upload_to='transaction/', blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.name} - {self.category_transition.name} - {self.payment_type.name}'
+    
+    class Meta:
+        db_table = 'transactions'
+
+
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f'{self.user.name} - Wallet Balance: {self.balance}'
+    
+    class Meta:
+        db_table = 'wallets'
+
